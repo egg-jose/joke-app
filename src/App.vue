b80e1e5a0ba3066fe4ffc59a7eb7a9004013e67d
Jose Esteban <josegg1897@gmail.com>
2022-12-01 05:49:05 -0400

    Create joke-app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container" :style="{'background-color': backgroundColor}">
+    <Joke :joke="joke"/>
+    <Button @btn-click="clickSearch" text="Get Random Joke" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Button';
+import Joke from './components/Joke';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Button,
+    Joke,
+  },
+  data() {
+    return {
+      joke: {},
+      colorCategory: {
+        "Programming": "#17BEBB",
+        "Pun": "#695958",
+        "Spooky": "#28464B",
+        "Christmas": "#931621",
+      },
+    }
+  },
+  methods: {
+    async getJoke() {
+      const res = await fetch('https://v2.jokeapi.dev/joke/Programming,Pun,Spooky,Christmas?type=twopart');
+      const data = await res.json();
+      return data;
+    },
+    async clickSearch() {
+      this.joke = await this.getJoke();
+    }
+  },
+  async created() {
+    this.joke =  await this.getJoke();
+  },
+  computed: {
+    backgroundColor() {
+      return this.colorCategory[this.joke.category]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Lexend+Deca&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Lexend Deca", sans-serif;
+  color: #0a2847;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.btn {
+  display: inline-block;
+  background: #0a2847;
+  color: #e9e9e9;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 60px;
+  font-family: inherit;
+}
+.btn:active {
+  transform: scale(0.98);
 }
 </style>
---
+++ src/components/Joke.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="joke-card">
+    <p class="category">Category: {{joke.category}}</p>
+    <div v-if="joke.type === 'single'">
+      <h1>{{joke.joke}}</h1>
+    </div>
+    <div v-if="joke.type === 'twopart'" >
+      <h1>{{joke.setup}}</h1>
+      <h1 :class="hidePunchLine ? 'blurry-text' : ''" @click="showText()">{{joke.delivery}}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Joke",
+  data() {
+    return {
+      hidePunchLine: true
+    }
+  },
+  props: {
+    joke: Object,
+  },
+  methods: {
+    showText() {
+      this.hidePunchLine = false;
+    }
+  },
+  watch: {
+    joke() {
+      this.hidePunchLine=true;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.joke-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  text-align: left;
+  border: 8px solid #0a2847;
+  border-radius: 20px;
+  padding: 40px;
+  background-color: #f0ffff;
+  max-width: 800px;
+}
+.category {
+  margin-bottom: 20px;
+}
+h1{
+  margin-bottom: 20px;
+}
+
+.blurry-text {
+  color: transparent;
+  text-shadow: 0 0 10px rgba(0,0,0,0.5);
+  cursor: pointer;
+}
+
+</style>
